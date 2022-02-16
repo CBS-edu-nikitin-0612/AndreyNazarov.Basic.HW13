8cfec58bedd21c298725c7ba0715d70ca1f861f5
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-16 23:00:42 +0300

    feat: Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Task1;
+using System.Threading;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            byte[] lockLine = new byte[Console.WindowWidth];
+            object lockConsole = new object();
+            var rnd = new Random();
+            while (true)
+            {
+                Thread.Sleep(30);
+                int i = rnd.Next(0, lockLine.Length);
+                if (lockLine[i] == 2)
+                {
+                    continue;
+                }
+                new Line(rnd.Next(4, 10), i, lockConsole, ref lockLine);
+            }
+        }
+    }
+}
